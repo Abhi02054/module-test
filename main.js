b6340aff1d91ed0762b68d82bc661d03516a2152
Abhi02054 <abhi02054@gmail.com>
2023-02-25 21:52:44 +0530

    yes

--- main.js
+++ main.js
@@ -1,59 +1,50 @@
-// Array to store issued books
-let issuedBooks = [];
+const addnewrowbutton = document.getElementById("but")
 
-// Function to issue a book
-function issueBook(event) {
-  event.preventDefault();
-  const bookName = document.getElementById("book-name").value;
-  const issuedTo = document.getElementById("issued-to").value;
-  const issuedTime = new Date().toLocaleString();
-  const id = issuedBooks.length + 1;
-  const status = "not returned";
-  issuedBooks.push({ id, bookName, issuedTo, issuedTime, status });
-  displayIssuedBooks();
-}
+const tablebody = document.getElementById("hello")
+const tableData = [];
 
-// Function to display issued books in the table
-function displayIssuedBooks() {
-  const tableBody = document.querySelector("#issued-books-table tbody");
-  tableBody.innerHTML = "";
-  issuedBooks.forEach(book => {
-    const row = document.createElement("tr");
-    const idCell = document.createElement("td");
-    idCell.textContent = book.id;
-    const bookNameCell = document.createElement("td");
-    bookNameCell.textContent = book.bookName;
-    const issuedToCell = document.createElement("td");
-    issuedToCell.textContent = book.issuedTo;
-    const issuedTimeCell = document.createElement("td");
-    issuedTimeCell.textContent = book.issuedTime;
-    const statusCell = document.createElement("td");
-    statusCell.textContent = book.status;
-    statusCell.classList.add(`status-${book.status.split(" ").join("-")}`);
-    statusCell.setAttribute("contenteditable", "true");
-    statusCell.addEventListener("blur", () => updateBookStatus(book.id));
-    row.appendChild(idCell);
-    row.appendChild(bookNameCell);
-    row.appendChild(issuedToCell);
-    row.appendChild(issuedTimeCell);
-    row.appendChild(statusCell);
-    tableBody.appendChild(row);
-  });
-}
 
-// Function to update the status of a book
-function updateBookStatus(id) {
-  const statusCell = document.querySelector(`#issued-books-table tbody tr:nth-child(${id}) td:nth-child(5)`);
-  const status = statusCell.textContent;
-  issuedBooks.forEach(book => {
-    if (book.id === id) {
-      book.status = status;
-      statusCell.classList.remove("status-returned", "status-not-returned");
-      statusCell.classList.add(`status-${status.split(" ").join("-")}`);
+
+function submitdata(name, roll, subject, marks, markedby) {
+    addnewrowbutton.disabled = false
+
+    if (name == "" || roll == "" || marks == "" || markedby == "" || subject == "") {
+        alert("Please Fill all the fields");
     }
-  });
+    else if (markedby.includes("@") == false) {
+        alert("Please Enter valid Email")
+    }
+    else {
+        let data = { id: tableData.length + 1, student_name: name, student_roll: roll, subject: subject, marks: marks, markedBy: markedby }
+        console.log("New Row Entered:", data);
+        tableData.push(data);
+        console.log("All Table Data", tableData);
+        tablebody.innerHTML = tableData.map((item, ind) => {
+            return (` <tr> <td>${item.id}</td>
+            <td>${item.student_name}</td>
+            <td>${item.student_roll}</td>
+            <td>${item.subject}</td>
+            <td>${item.marks}</td>
+            <td>${item.markedBy}</td></tr>`)
+        }).join("")
+
+    }
+
 }
 
-// Add event listener to the issue form
-const issueForm = document.getElementById("issue-form");
-issueForm.addEventListener("submit", issueBook);
+
+addnewrowbutton.addEventListener("click", () => {
+
+    tablebody.innerHTML = tablebody.innerHTML + `
+  <td>${tableData.length + 1}</td>
+  <form >
+  <td><input id="stuname" type="text"/></td>
+  <td><input id="rollno" type="text"/></td>
+  <td><input id="subject" type="text"/></td>
+  <td><input id="marks" type="number"/></td>
+  <td><input id="markedby" type="email"/></td>
+  <td><button class="savebutt" onclick="submitdata(document.getElementById('stuname').value,document.getElementById('rollno').value,document.getElementById('subject').value,document.getElementById('marks').value,document.getElementById('markedby').value)">Save</button></td>
+  </form>`
+
+    addnewrowbutton.disabled = true
+})
