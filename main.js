a6178aa05bacce9679ab0c4bc08d9cb5c8ffea96
Abhi02054 <abhi02054@gmail.com>
2023-03-04 21:32:15 +0530

    ok

--- main.js
+++ main.js
@@ -1,121 +1,175 @@
-const score = document.querySelector(".score");
-const startScreen = document.querySelector(".startScreen");
-const gameArea = document.querySelector(".gameArea");
-let player = { speed: 5, score: 0 };
-let keys = {
-  ArrowUp: false,
-  ArrowDown: false,
-  ArrowRight: false,
-  ArrowLeft: false,
-};
-startScreen.addEventListener("click", start);
-document.addEventListener("keydown", pressOn);
-document.addEventListener("keyup", pressOff);
-function moveLines() {
-  let lines = document.querySelectorAll(".line");
-  lines.forEach(function (item) {
-    if (item.y > 1500) {
-      item.y -= 1500;
-    }
-    item.y += player.speed;
-    item.style.top = item.y + "px";
-  });
-}
-function isCollide(a, b) {
-  aRect = a.getBoundingClientRect();
-  bRect = b.getBoundingClientRect();
-  return !(
-    aRect.bottom < bRect.top ||
-    aRect.top > bRect.bottom ||
-    aRect.right < bRect.left ||
-    aRect.left > bRect.right
-  );
-}
-function moveEnemy(car) {
-  let ele = document.querySelectorAll(".enemy");
-  ele.forEach(function (item) {
-    if (isCollide(car, item)) {
-      endGame();
-    }
-    if (item.y > 1500) {
-      item.y = -600;
-      item.style.left = Math.floor(Math.random() * 350) + "px";
-    }
-    item.y += player.speed;
-    item.style.top = item.y + "px";
-  });
-}
-function playGame() {
-  let car = document.querySelector(".car");
-  let road = gameArea.getBoundingClientRect();
+let saveBlogbut = document.getElementById('saveBlogbut');
+let text1 = document.getElementById('exampleFormControlInput1');
+let text2 = document.getElementById('exampleFormControlTextarea1')
+let postbody = document.getElementById("postbody");
+let editmodal = document.getElementById('exampleModal2');
 
-  moveLines();
-  moveEnemy(car);
-  if (player.start) {
-    if (keys.ArrowUp && player.y > road.top - 542) {
-      player.y -= player.speed;
+let dataArr = [];
+function datetimerformatter(data) {
+    const arr = data.split(",");
+    const time = arr[1].split(":");
+    if (parseInt(time[0]) >= 12) {
+        const tval = parseInt(time[0]) == 12 ? "12" : time[0] % 12
+        return arr[0] + "  at " + tval + ":" + time[1] + " PM";
     }
-    if (keys.ArrowDown && player.y < road.bottom - 237) {
-      player.y += player.speed;
+    else {
+        return arr[0] + "  at " + time[0] + ":" + time[1] + " AM";
     }
-    if (keys.ArrowLeft && player.x > 0) {
-      player.x -= player.speed;
-    }
-    if (keys.ArrowRight && player.x < road.width - 54) {
-      player.x += player.speed;
-    }
-    car.style.left = player.x + "px";
-    car.style.top = player.y + "px";
-    window.requestAnimationFrame(playGame);
-    player.score++;
-    score.innerText = "Score : " + player.score;
-  }
 }
-function pressOn(e) {
-  e.preventDefault();
-  keys[e.key] = true;
+
+
+function deletedata(id) {
+
+    dataArr = dataArr.filter((item, ind) => {
+        return item.id != id;
+    })
+
+    postbody.innerHTML = dataArr.map((item, ind) => {
+        return (` <div class="row me-4 ms-4 my-4 p-3 border border-dark " >
+        <div class="heading my-2">
+          <h1>${item.heading}</h1>
+        </div>
+
+        <div class="contents">
+        ${item.post}
+        </div>
+
+        <div class="dateshower">
+            <div class="my-4">
+                <button type="button" id=${item.id} onclick=editdata(this.id) class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+                     Edit Post
+                 </button>
+                   <button type="button" id=${item.id} onclick=deletedata(this.id) class="btn btn-dark">Delete Post</button>
+                 </div>
+        
+        
+                  <div >
+                  ${item.text}: ${datetimerformatter(new Date().toLocaleString())}
+                 </div>
+        
+        </div>
+        </div> `)
+    }).join("")
+
+    console.log(dataArr)
 }
-function pressOff(e) {
-  e.preventDefault();
-  keys[e.key] = false;
+
+function editsavepost(data1, data2, id) {
+
+    console.log(data1, data2, id)
+    const data = { id: id, heading: data1, post: data2, text: "Last Updated At" }
+    dataArr = dataArr.map((item, ind) => {
+        if (item.id == id) {
+            return data;
+        }
+        else {
+            return item;
+        }
+    })
+
+    postbody.innerHTML = dataArr.map((item, ind) => {
+        return (` <div class="row me-4 ms-4 my-4 p-3 border border-dark " >
+        <div class="heading my-2">
+          <h1>${item.heading}</h1>
+        </div>
+
+        <div class="contents">
+        ${item.post}
+        </div>
+
+        <div class="dateshower">
+            <div class="my-4">
+                <button type="button" id=${item.id} onclick=editdata(this.id) class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+                     Edit Post
+                 </button>
+                   <button type="button" id=${item.id} onclick=deletedata(this.id) class="btn btn-dark">Delete Post</button>
+                 </div>
+        
+        
+                  <div >
+                 ${item.text}: ${datetimerformatter(new Date().toLocaleString())}
+                 </div>
+        
+        </div>
+        </div> `)
+    }).join("")
+    console.log(dataArr)
+
 }
-function endGame() {
-  player.start = false;
-  score.innerHTML = "Game Over<br>Score :  " + player.score;
-  startScreen.classList.remove("hide");
+
+function editdata(id) {
+    const data = dataArr.filter((item, ind) => {
+        return item.id == id
+    })
+
+
+
+    editmodal.innerHTML = `<div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Edit a new post</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+          <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label"></label>
+              <input type="text" class="form-control form-control-lg" id="exampleFormControlInputedit1" placeholder="Enter Your Heading" value=${data[0].heading} >
+            </div>
+            <div class="mb-3">
+              <label for="exampleFormControlTextarea1" class="form-label"></label>
+              <textarea class="form-control form-control-lg" id="exampleFormControlTextareaedit2" rows="3" placeholder="Enter Your Blog Post">${data[0].post}</textarea>
+            </div>
+      </div>
+      <div class="modal-footer">
+       
+        <button type="button" id="saveBlogbut" onclick=editsavepost(document.getElementById('exampleFormControlInputedit1').value,document.getElementById('exampleFormControlTextareaedit2').value,${id}) data-bs-dismiss="modal" class="btn btn-dark">Save Post</button>
+        <button type="button" class="btn btn-dark" onclick=deletedata(id) data-bs-dismiss="modal">Delete Post</button>
+      </div>
+    </div>
+    </div> `
+
 }
-function start() {
-  startScreen.classList.add("hide");
-  gameArea.innerHTML = "";
-  player.start = true;
-  player.score = 0;
-  for (let x = 0; x < 10; x++) {
-    let div = document.createElement("div");
-    div.classList.add("line");
-    div.y = x * 150;
-    div.style.top = x * 150 + "px";
-    gameArea.appendChild(div);
-  }
-  window.requestAnimationFrame(playGame);
-  let car = document.createElement("div");
-  car.setAttribute("class", "car");
-  gameArea.appendChild(car);
-  player.x = car.offsetLeft;
-  player.y = car.offsetTop;
-  for (let x = 0; x < 3; x++) {
-    let enemy = document.createElement("div");
-    enemy.classList.add("enemy");
-    enemy.y = (x + 1) * 600 * -1;
-    enemy.style.top = enemy.y + "px";
-    enemy.style.left = Math.floor(Math.random() * 350) + "px";
-    enemy.style.backgroundColor = randomColor();
-    gameArea.appendChild(enemy);
-  }
-}
-function randomColor() {
-  function c() {
-    let hex = Math.floor(Math.random() * 256).toString(16);
-    return ("0" + String(hex)).substr(-2);
-  }
-  return "#" + c() + c() + c();
-}
+
+
+
+saveBlogbut.addEventListener("click", function () {
+
+    const data = { id: dataArr.length + 1, heading: text1.value, post: text2.value, text: "Created At" }
+
+    dataArr.push(data)
+
+    postbody.innerHTML = dataArr.map((item, ind) => {
+        return (`  <div class="row me-4 ms-4 my-4 p-3 border border-dark " >
+        <div class="heading my-2">
+          <h1>${item.heading}</h1>
+        </div>
+
+        <div class="contents">
+        ${item.post}
+        </div>
+
+        <div class="dateshower">
+            <div class="my-4">
+                <button type="button" id=${item.id} onclick=editdata(this.id) class="btn btn-dark  my-2" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+                     Edit Post
+                 </button>
+                   <button type="button" id=${item.id} onclick=deletedata(this.id) class="btn btn-dark my-2">Delete Post</button>
+                 </div>
+        
+        
+                  <div >
+                  ${item.text}: ${datetimerformatter(new Date().toLocaleString())}
+                 </div>
+        
+        </div>
+        </div>
+        
+        `)
+    }).join("")
+
+
+
+})
+
+
+
